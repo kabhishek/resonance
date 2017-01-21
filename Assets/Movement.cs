3974d7664515ba9b964f491062e785c2616e6471
Shringi <sk1382@york.ac.uk>
2017-01-21 15:46:33 +0000

    Add npc spawner, trail prefab. Make npc spawner spawn based on some logic

---
+++ Assets/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+     private Rigidbody2D rb;
+     private float theta = 0f;
+     private float thetaStep = 0.5f;
+     public float speed = 0.01f;
+     float amplitudeX = 10.0f;
+     float amplitudeY = 1.0f;
+     float omegaX = 1.0f;
+     float omegaY = 1.0f;
+     float index;
+	// Use this for initialization
+	void Start () {
+
+          rb = gameObject.GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     private void FixedUpdate()
+     {
+          theta += thetaStep;
+          float localX = transform.position.x;
+          localX -= Time.fixedDeltaTime;
+          //float localY = transform.position.y;
+          //thetaStep *= Time.fixedDeltaTime;
+          //localY += Mathf.Sin (theta) * speed;
+          //transform.position = new Vector2 (localX, localY);
+
+
+
+          /*theta += thetaStep;
+          Vector3 referenceVector = new Vector2 (0.01f, 
+               Mathf.Sin (theta) * 0.01f);
+          rb.velocity = referenceVector.normalized * Time.fixedDeltaTime * speed;*/
+          //rb.angularVelocity = 20.0f;
+
+
+
+
+          index += Time.fixedDeltaTime;
+          //float x = amplitudeX*Mathf.Cos (omegaX*index);
+          //float y = amplitudeY * Mathf.Sin (omegaY*index);
+          transform.position= new Vector3(localX,transform.position.y);
+          //transform.position= new Vector3(localX,y) * speed;
+     }
+}
---
+++ Assets/NPC_Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Spawner : MonoBehaviour {
+
+     [SerializeField] private GameObject npcPrefab;
+
+     private List<GameObject> spawnedNPCs = new List<GameObject>();
+
+     // Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+     private void Update () {
+	}
+
+     public void Spawn()
+     {
+          Vector2 spawnPositionInViewPort = new Vector2 (1, Random.Range (0.0f, 1.0f));
+          Vector2 spawnPositionInWorld = Camera.main.ViewportToWorldPoint (spawnPositionInViewPort);
+          GameObject local = GameObject.Instantiate<GameObject> (npcPrefab, spawnPositionInWorld, Quaternion.identity);
+          spawnedNPCs.Add (local);
+     }
+}
