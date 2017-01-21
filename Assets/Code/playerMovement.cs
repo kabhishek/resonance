02d1e1709c12241a3d89f9a11be38274f237f812
kispygr <kispygr@gmail.com>
2017-01-21 20:14:36 +0000

    Story Display basic funtionality to be MOVED to collition done

--- Assets/Code/playerMovement.cs
+++ Assets/Code/playerMovement.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerMovement : MonoBehaviour {
 
 	public float speed;
+	public GameObject canvasStory;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +22,13 @@ public class playerMovement : MonoBehaviour {
 		} else if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S)) {
 			// Particle goes down
 			transform.Translate (-Vector2.up * speed * Time.deltaTime);
+		}
+
+		// THIS IS A TEST. REMOVE WHEN ADDED TO COLLIDER
+		if (Input.GetKey (KeyCode.B)) {
+			
+			canvasStory.SetActive(true);
+			GameObject.FindGameObjectWithTag ("StoryText").GetComponent<Text> ().text = "Lalal";
 		}
 	}
 }
