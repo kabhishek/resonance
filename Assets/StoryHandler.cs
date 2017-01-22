6bd7614988c1c9090097173d249a124f49a46651
kispygr <kispygr@gmail.com>
2017-01-22 12:09:21 +0000

    Bg color change

--- Assets/StoryHandler.cs
+++ Assets/StoryHandler.cs
@@ -7,11 +7,16 @@ public class StoryHandler : MonoBehaviour {
 
 	[SerializeField] private MonoBehaviour collisionInfo;
 	[SerializeField] private GameObject canvasStory;
+
 	private ICollisionInfo iCollisionInfo; 
+	private Color bgColor;
+
 	// Use this for initialization
 	void Start () {
 		iCollisionInfo = collisionInfo.GetComponent<ICollisionInfo> ();
 		iCollisionInfo.PlayerCollision.AddListener (UpdateStory);
+
+		bgColor = new Color ();
 	}
 	
 	// Update is called once per frame
@@ -21,9 +26,30 @@ public class StoryHandler : MonoBehaviour {
 
 	private void UpdateStory(float state)
 	{
-		Debug.Log ("Story " + state);
+		UpdateEnvironment (state);
+	
 		canvasStory.SetActive(true);
 		GameObject.FindGameObjectWithTag ("StoryText").GetComponent<Text> ().text = "These are cautiously pleated waves \n of us, of you.\n Shall we? \nas we resonate.";
+	}
+
+	void UpdateEnvironment(float state){
+		switch ((int)state) 
+		{
+		case 10:
+		case -10:
+			ColorUtility.TryParseHtmlString ("#48489AFF", out bgColor);
+			break;
+		case 30:
+		case -30:
+			ColorUtility.TryParseHtmlString ("#B82727FF", out bgColor);
+			break;
+		default:
+			ColorUtility.TryParseHtmlString ("#424242FF", out bgColor);
+			break;
+		}
+
+		GameObject.Find ("Background").GetComponent<SpriteRenderer> ().color = bgColor;
+		GameObject.Find("Background(Clone)").GetComponent<SpriteRenderer> ().color = bgColor;
 	}
 		
 }
